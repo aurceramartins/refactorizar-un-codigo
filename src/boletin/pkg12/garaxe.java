7fc92e178a99c1f4846f57bbc4bdf33b8382c47e
aurceramartins <aurceramartins@localhost>
2014-12-01 09:08:07 +0100

    Cambio de la variable matriculla por tiempo inicial

--- src/boletin/pkg12/garaxe.java
+++ src/boletin/pkg12/garaxe.java
@@ -6,19 +6,19 @@ public class garaxe {
 
     public garaxe() {
     }
-    private long matricula;
+    private long tiempoinicial;
     private long tiemposalida;
     double tiempoeuros;
 
     public void numerodecoches() {
         JOptionPane.showInputDialog("Dime la matricula del coche");
-        matricula = System.currentTimeMillis();
+        tiempoinicial = System.currentTimeMillis();
     }
 
     public void contadoreuros() {
         JOptionPane.showMessageDialog(null, "Introduce el tikect");
         tiemposalida = System.currentTimeMillis();
-        long resultado = (tiemposalida - matricula);
+        long resultado = (tiemposalida - tiempoinicial);
 
         tiempoeuros = (((resultado / 3) - 1) * 0.2) + 0.4;
         JOptionPane.showMessageDialog(null, "Usted ha tardado " + (resultado / 1000) + " segundos en retirar su coche \n Debe pagar " + tiempoeuros / 1000 + " euros");
